a6b0b2063b091e254a90aa52a29b4f157a79bcda
marc desrosiers <mdesrosiers@norauto.fr>
2016-08-21 01:21:26 +0200

    start to work on order details

--- order_detail.php
+++ order_detail.php
@@ -1,4 +1,20 @@
 <?php include ("php_file/fonctions.php"); ?>
 <?php include ("layouts/orders/top.php"); ?>
 
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-md-6">
+      <div class="information">
+        <h2>Customer zone</h2>
+      </div>
+    <div class="col-xs-12 col-md-6">
+      <div class="information">
+        <h2>Shipping zone</h2>
+      </div>
+
+    </div>
+    </div>
+  </div>
+</div>
+
 <?php include ("layouts/orders/bottom.php") ?>
