a37f9ad444b3b4cb30e86fa1c1bab26c6d3b93be
marc desrosiers <mdesrosiers@norauto.fr>
2016-09-16 23:05:19 +0200

    add a alert for password error

--- js/global.js
+++ js/global.js
@@ -35,6 +35,14 @@ $(document).ready(function(){
     else
       {$('#mail-alert').slideUp();}
   });
+  $('#namePasswordVerification').blur(function(){
+    var password_first = $('#namePassword')[0].value;
+    var password_second = $('#namePasswordVerification')[0].value;
+    if (password_first != password_second)
+      {$('#password-alert').slideDown();}
+    else
+      {$('#password-alert').slideUp();}
+  });
 
 
 });
