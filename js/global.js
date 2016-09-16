f4042b5681b0c544532682ab277ba03f4a04c760
marc desrosiers <mdesrosiers@norauto.fr>
2016-09-16 22:17:04 +0200

    Work on alert for difference between two email wrote

--- js/global.js
+++ js/global.js
@@ -23,9 +23,17 @@
 //   window.location.href= "#" + target;
 // };
 
-$(document).ready(function() {
+$(document).ready(function(){
   $('#button_to_animate').click(function(){
     $('#h2animate').addClass('animated hinge');
+  });
+  $('#mailClientVerification').blur(function(){
+    var mail_first = $('#mailClient')[0].value;
+    var mail_second = $('#mailClientVerification')[0].value;
+    if (mail_first != mail_second)
+      {$('#mail-alert').slideDown();}
+    else
+      {$('#mail-alert').slideUp();}
   });
 
 
